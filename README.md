5853f4b55738a8726f0028d096d00ddc95e8b80c
Brian Earl <bearl@mastercontrol.com>
2023-11-26 16:58:45 -0700

    Inserted the SQL data into an HTML table, displayed it on the web page and copied it to the clipboard.

---
+++ README.md
@@ -0,0 +1,22 @@
+# TabEnterReformatter
+A webpage where users can paste their SQL outputs or Excel data, (This should handle anything that uses tab delimiters between the columns and Carriage Returns - CRLF ) and the data will be reformatted with underscores to right align each column.
+
+example data copied from sql table:
+```
+id	name	zipcode	phone
+0	Bill	90210	8585552485
+1	Columbus	84101	9315552938
+2	Sir Fredrick The Third	10011	8085553374
+3	Jane	57493	9685551929
+4	Jimothy	92106	2015552293
+```
+
+after formatting:
+```
+id	name	                zipcode	phone
+0	Bill	                90210	8585552485
+1	Columbus	        84101	9315552938
+2	Sir Fredrick The Third	10011	8085553374
+3	Jane	                57493	9685551929
+4	Jimothy	                92106	2015552293
+```
---
+++ index.js
@@ -0,0 +1,122 @@
+jQuery(document).ready(function() 
+{
+    $("#formForUglySql").submit(function(event)
+    {
+        event.preventDefault();
+        
+        //get the SQL statement
+        let uglySql = $("#usersSqlStatement").val();
+
+        //let $answerField = $("#results1");
+        
+        //separate the rows of the SQL statement
+        var rowArray = uglySql.split("\n");
+
+        //iterate through each row saved in the rowArray
+        //push the column value into an array of arrays
+        var columnArray = [];
+v       //create a 2D array from the SQL data to iterate through each row and column
+        for (let x of rowArray)
+        {
+            columnArray.push(x.split("\t"));
+        }
+        //concatenate a code block and a table element at the beginning of the clipboard string so the formatting appears in ServiceNow
+        let clipboardString = "[code]<table>";
+        //iterate through each row
+        for (let y = 0; y < columnArray.length; y++) 
+        {
+            //at the start of each row, concatenate a row element to the clipboardString
+            clipboardString += "<tr>";
+            //at the start of each row, append a row element to the results1 table element
+            $("#results1").append("<tr>"); 
+            //iterate through each column of each row
+            for (let z = 0; z < columnArray[y].length; z++)
+            {
+                //if on the first row...
+                if (y == 0){
+                    //concatenate a table header element to the clipboardString
+                    clipboardString += "<th style='text-wrap:nowrap'>" + columnArray[y][z] + "</th>";
+                    //append a table header element to the results1 table element
+                    $("#results1").append("<th style='text-wrap:nowrap'>" + columnArray[y][z] + "</th>");
+                }
+                //if not on the first row...
+                if (y != 0){
+                    //concatenate a table data cell element to the clipboardString
+                    clipboardString += "<td>" + columnArray[y][z] + "</td>";
+                    //append a table data cell element to the results1 table element
+                    $("#results1").append("<td>" + columnArray[y][z] + "</td>");
+                }   
+            }
+            //at the end of each row, concatenate a closing row element to the clipboardString
+            clipboardString += "</tr>";
+            //at the end of each row, append a closing row element to the results1 table element
+            $("#results1").append("</tr>"); 
+        }
+        //concatenate a closing table element and a code block at the end of the clipboard string so the formatting appears in ServiceNow
+        clipboardString += "</table>[code]";
+        //Copy the clipboardString to the clipboard
+        navigator.clipboard.writeText(clipboardString);
+
+        /*const displayValueOutput = (columnMax, cellValue) => {
+            let outputString = cellValue.toUpperCase();
+            
+            //Max string length to 25 chars
+            if (columnMax > 25)
+            {
+                outputString.substring(0, 25);
+                columnMax = 25;
+            }
+            
+            if (outputString.length == columnMax)
+            {
+                $answerField.append("<div>" + outputString + "_" + "</div>") 
+            
+                moveToNextColumnCheck();
+            }
+            else
+            {
+                addWhiteSpace(outputString , columnMax);
+            }
+        }*/
+
+        //determine the largest amount of characters appearing in each column
+        /*function calculateMaxOfEachColumn()
+        {
+            //iterate through each cell value for a column and insert the lengths into
+            // columnStingslengths array
+            for (let column = 0; column < columnArray.length; column++)
+            {
+                for (let cell = 0; cell < columnArray[0].length; cell++)
+                {
+                    columnStringLength.push(columnArray[column][cell].length);
+                    console.log(columnStringLength);
+                }
+            }
+                // let maxCharPerColumn = Math.max.apply(null, columnStringLength);
+                //let maxCharPerColumn = Math.max(...columnArray[]);//es6 spread operator
+               // console.log('maxCharPerColumn' , maxCharPerColumn);
+                columnMax.push(maxCharPerColumn);
+                displayValueOutput(columnMax[row], cellValue[row]);
+                if (cellValue.length >= columnArray.length)
+                {
+                    columnStringLength = [];
+                    cellValue = [];
+                }
+        }
+        let columnMax = [];
+        let columnStringLength = [];
+        
+            //get the first element of array [0][0] and add whitespace up to the value columnMax[0] 
+            
+        function addWhiteSpace(outputString, columnMax)
+        {
+            let numberOfSpacesToAdd = columnMax - outputString.length;
+            let spaces = "";
+            spaces = "&#95;".repeat(numberOfSpacesToAdd) + spaces;
+            let rightJustifiedValue = spaces + outputString;
+            $answerField.append("<div>" + rightJustifiedValue + "_" + "</div>") 
+
+            moveToNextColumnCheck();
+        }*/
+    })
+})
