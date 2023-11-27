4faf15bf19fca1e9dfb4f1d72c84d1556aac1351
Brian Earl <bearl@mastercontrol.com>
2023-11-27 15:16:09 -0700

    Inserted the SQL data into an HTML table, displayed it on the web page and copied it to the clipboard. Also, set a Max for column Characters.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function()
         //get the SQL statement
         let uglySql = $("#usersSqlStatement").val();
 
-        let $answerField = $("#results1");
+        //let $answerField = $("#results1");
 
         //separate the rows of the SQL statement
         var rowArray = uglySql.split("\n");
@@ -16,7 +16,59 @@ jQuery(document).ready(function()
         //push the column value into an array of arrays
         var columnArray = [];
 
-        const displayValueOutput = (columnMax, cellValue) => {
+        for (let x of rowArray)
+        {
+            columnArray.push(x.split("\t"));
+        }
+        //SETS THE MAX CHARACTERS FOR A COLUMN
+        //let max = 25;
+        //concatenate a code block and a table element at the beginning of the clipboard string so the formatting appears in ServiceNow
+        let clipboardString = "[code]<table>";
+        //iterate through each row
+        for (let y = 0; y < columnArray.length; y++) 
+        {
+            //at the start of each row, concatenate a row element to the clipboardString
+            clipboardString += "<tr>";
+            //at the start of each row, append a row element to the results1 table element
+            $("#results1").append("<tr>"); 
+            //iterate through each column of each row
+            for (let z = 0; z < columnArray[y].length; z++)
+            {
+                /* THIS CODE SETS A MAX AMOUNT OF CHARACTERS FOR A COLUMN
+                characterArray.push(columnArray[y][z].split(''));
+                if (columnArray[y][z].length > max) {
+                    let shortened = '';
+                    for (let w = 0; w < max; w++){
+                        shortened += characterArray[(y*4)+z][w];
+                        columnArray[y][z] = shortened; 
+                    }
+                }*/
+                //if on the first row...
+                if (y == 0){
+                    //concatenate a table header element to the clipboardString
+                    clipboardString += "<th style='text-wrap:nowrap'>" + columnArray[y][z] + "</th>";
+                    //append a table header element to the results1 table element
+                    $("#results1").append("<th style='text-wrap:nowrap'>" + columnArray[y][z] + "</th>");
+                }
+                //if not on the first row...
+                if (y != 0){
+                    //concatenate a table data cell element to the clipboardString
+                    clipboardString += "<td>" + columnArray[y][z] + "</td>";
+                    //append a table data cell element to the results1 table element
+                    $("#results1").append("<td>" + columnArray[y][z] + "</td>");
+                }   
+            }
+            //at the end of each row, concatenate a closing row element to the clipboardString
+            clipboardString += "</tr>";
+            //at the end of each row, append a closing row element to the results1 table element
+            $("#results1").append("</tr>"); 
+        }
+        //concatenate a closing table element and a code block at the end of the clipboard string so the formatting appears in ServiceNow
+        clipboardString += "</table>[code]";
+        //Copy the clipboardString to the clipboard
+        navigator.clipboard.writeText(clipboardString);
+
+        /*const displayValueOutput = (columnMax, cellValue) => {
             let outputString = cellValue.toUpperCase();
             
             //Max string length to 25 chars
@@ -85,6 +137,6 @@ jQuery(document).ready(function()
             $answerField.append("<div>" + rightJustifiedValue + "_" + "</div>") 
 
             moveToNextColumnCheck();
-        }
+        }*/
     })
 })
