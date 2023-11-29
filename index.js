849e05aea1a52a0c83a37c8edba1b3d81605ffa1
Brian Earl <bearl@mastercontrol.com>
2023-11-28 22:44:44 -0700

    changed max column function to use column length instead of 4. Added border:1px solid to table and td elements.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ jQuery(document).ready(function()
         var characterArray = [];
         let max = 25;
         //concatenate a code block and a table element at the beginning of the clipboard string so the formatting appears in ServiceNow
-        let clipboardString = "[code]<table>";
+        let clipboardString = "[code]<table style='border:1px solid'>";
         //iterate through each row
         for (let y = 0; y < columnArray.length; y++) 
         {
@@ -40,7 +40,7 @@ jQuery(document).ready(function()
                 if (columnArray[y][z].length > max) {
                     let shortened = '';
                     for (let w = 0; w < max; w++){
-                        shortened += characterArray[(y*4)+z][w];
+                        shortened += characterArray[(y * columnArray[y].length) + z][w];
                         columnArray[y][z] = shortened; 
                     }
                 }
@@ -49,14 +49,14 @@ jQuery(document).ready(function()
                     //concatenate a table header element to the clipboardString
                     clipboardString += "<th style='text-wrap:nowrap'>" + columnArray[y][z] + "</th>";
                     //append a table header element to the results1 table element
-                    $("#results1").append("<th style='text-wrap:nowrap'>" + columnArray[y][z] + "</th>");
+                    $("#results1").append("<th style='text-wrap:nowrap,text-align:left'>" + columnArray[y][z] + "</th>");
                 }
                 //if not on the first row...
                 if (y != 0){
                     //concatenate a table data cell element to the clipboardString
-                    clipboardString += "<td>" + columnArray[y][z] + "</td>";
+                    clipboardString += "<td style='border:1px solid'>" + columnArray[y][z] + "</td>";
                     //append a table data cell element to the results1 table element
-                    $("#results1").append("<td>" + columnArray[y][z] + "</td>");
+                    $("#results1").append("<td style='border:1px solid'>" + columnArray[y][z] + "</td>");
                 }   
             }
             //at the end of each row, concatenate a closing row element to the clipboardString
